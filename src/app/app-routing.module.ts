5f16de376d88d200676fbce52a740192303e129d
Himanshu Jain <himanshu.jain@pristyncare.com>
2022-05-26 01:50:30 +0530

    Adding basic Configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+      path: 'user',
+      component: UserLoginComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
